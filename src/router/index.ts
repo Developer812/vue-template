4ec261af5398f9ae0632fc2a5306525b71c61c2f
guojialu <2yuecloud@gmail.com>
2024-09-24 16:40:03 +0800

    fix: 代码修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/views/home/index.vue'
+import Layout from '@/layout/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomePage,
+      name: '/',
+      redirect: '/home',
+      component: Layout,
+      children: [
+        {
+          path: '/home',
+          name: 'Home',
+          component: () => import('@/views/home/index.vue'),
+        },
+      ],
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('@/views/AboutPage.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'Error',
+      component: () => import('@/views/error/404.vue'),
     },
   ],
 })
