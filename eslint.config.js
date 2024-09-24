d07797ac918753ad505c5dde4888a914759b1887
guojialu <2yuecloud@gmail.com>
2024-09-24 16:39:49 +0800

    fix: 代码修改

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,75 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+  {
+    ...js.configs.recommended,
+    ignores: [
+      '**/.*',
+      'dist/*',
+      '*.d.ts',
+      'public/*',
+      'src/assets/**',
+      'src/**/iconfont/**',
+    ],
+    languageOptions: {
+      globals: {
+        // index.d.ts
+        RefType: 'readonly',
+        EmitType: 'readonly',
+        TargetContext: 'readonly',
+        ComponentRef: 'readonly',
+        ElRef: 'readonly',
+        ForDataType: 'readonly',
+        AnyFunction: 'readonly',
+        PropType: 'readonly',
+        Writable: 'readonly',
+        Nullable: 'readonly',
+        NonNullable: 'readonly',
+        Recordable: 'readonly',
+        ReadonlyRecordable: 'readonly',
+        Indexable: 'readonly',
+        DeepPartial: 'readonly',
+        Without: 'readonly',
+        Exclusive: 'readonly',
+        TimeoutHandle: 'readonly',
+        IntervalHandle: 'readonly',
+        Effect: 'readonly',
+        ChangeEvent: 'readonly',
+        WheelEvent: 'readonly',
+        ImportMetaEnv: 'readonly',
+        Fn: 'readonly',
+        PromiseFn: 'readonly',
+        ComponentElRef: 'readonly',
+        parseInt: 'readonly',
+        parseFloat: 'readonly',
+      },
+    },
+    rules: {
+      'no-debugger': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
   {
     files: ['**/*.vue'],
+    plugins: {
+      vue: pluginVue,
+    },
     languageOptions: { parserOptions: { parser: tseslint.parser } },
     rules: { 'vue/multi-word-component-names': 'off' },
+  },
+  {
+    files: ['**/*.?([cm])js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+    },
   },
 ]
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import './styles/index.scss'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// 引入公共样式
+import './styles/index.scss'
+// 引入tailwind样式
+import './styles/tailwind.scss'
+// 引入字体图标样式
+import './assets/iconfont/iconfont'
+import './assets/iconfont/iconfont.css'
 
 const pinia = createPinia()
 const app = createApp(App)
